4f44b80ec56fc7ac3f104b46ac9feb772a6f9d45
Brian Cline <brian.cline@gmail.com>
2012-10-08 22:09:19 -0500

    Add gitconfig to install

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -fs ~/env/.zshrc ~/.zshrc
 ln -fs ~/env/.tmux.conf ~/.tmux.conf
 ln -fs ~/env/.vimrc ~/.vimrc
 ln -fs ~/env/vim ~/.vim
+ln -fs ~/env/.gitconfig-home ~/.gitconfig
 
 chsh -s $(which zsh)
 
