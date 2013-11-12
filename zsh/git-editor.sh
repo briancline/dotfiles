27892a451f7f77702a4ccbf931fa60c7c1c0007d
Brian Cline <brian.cline@gmail.com>
2013-11-12 01:59:57 -0600

    Fix issue with git pager and editor

--- zsh/git-editor.sh
+++ zsh/git-editor.sh
@@ -1,6 +1,7 @@
-if [ -f "$HOME/bin/st" ]; then
+if [ -f "$HOME/bin/st" ] && [[ "${OSTYPE}" =~ "linux" ]]; then
     git config --global core.editor "~/bin/st -w"
+elif [ -f "$HOME/bin/st" ]; then
+    git config --global core.editor "~/bin/st"
 else
     git config --global core.editor "vim"
 fi
-
