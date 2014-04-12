e2f7885959ffb0cc6ee64a56ff1c7dc35b95e160
Brian Cline <brian.cline@gmail.com>
2014-04-12 13:37:12 -0500

    Minor tweaks to git-editor.sh

--- zsh/git-editor.sh
+++ zsh/git-editor.sh
@@ -6,34 +6,36 @@ if [ ! -f "$HOME_EDITOR" ] || [ ! -f "$HOME_EDITOR_SUBL" ]; then
 
     OSX_SUBLIME_BIN="/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
     LINUX_SUBLIME_BIN="$(which subl 2>/dev/null)"
-    LINUX_SUBLIME_HOME_BIN="$HOME/apps/sublime-3/sublime_text"
+    LINUX_SUBLIME_HOME_BIN="$HOME/apps/sublime/sublime_text"
     LINUX_SUBLIME_OPT_BIN="/opt/sublime_text/sublime_text"
     VIM_LOCATION="$(which vim 2>/dev/null)"
     VI_LOCATION="$(which vi 2>/dev/null)"
     NANO_LOCATION="$(which nano 2>/dev/null)"
+    PICO_LOCATION="$(which pico 2>/dev/null)"
 
     if [ -f "$OSX_SUBLIME_BIN" ]; then
-        ln -fs $OSX_SUBLIME_BIN $HOME_EDITOR
-        ln -fs $OSX_SUBLIME_BIN $HOME_EDITOR_SUBL
+        ln -fs "$OSX_SUBLIME_BIN" $HOME_EDITOR
+        ln -fs "$OSX_SUBLIME_BIN" $HOME_EDITOR_SUBL
     elif [ -f "$LINUX_SUBLIME_OPT_BIN" ]; then
-        ln -fs $LINUX_SUBLIME_OPT_BIN $HOME_EDITOR
-        ln -fs $LINUX_SUBLIME_OPT_BIN $HOME_EDITOR_SUBL
+        ln -fs "$LINUX_SUBLIME_OPT_BIN" $HOME_EDITOR
+        ln -fs "$LINUX_SUBLIME_OPT_BIN" $HOME_EDITOR_SUBL
     elif [ -f "$LINUX_SUBLIME_BIN" ]; then
-        ln -fs $LINUX_SUBLIME_BIN $HOME_EDITOR
-        ln -fs $LINUX_SUBLIME_BIN $HOME_EDITOR_SUBL
+        ln -fs "$LINUX_SUBLIME_BIN" $HOME_EDITOR
+        ln -fs "$LINUX_SUBLIME_BIN" $HOME_EDITOR_SUBL
     elif [ -f "$LINUX_SUBLIME_HOME_BIN" ]; then
-        ln -fs $LINUX_SUBLIME_HOME_BIN $HOME_EDITOR
-        ln -fs $LINUX_SUBLIME_HOME_BIN $HOME_EDITOR_SUBL
+        ln -fs "$LINUX_SUBLIME_HOME_BIN" $HOME_EDITOR
+        ln -fs "$LINUX_SUBLIME_HOME_BIN" $HOME_EDITOR_SUBL
     elif [ "$VIM_LOCATION" ]; then
         ln -fs $VIM_LOCATION $HOME_EDITOR
     elif [ "$VI_LOCATION" ]; then
         ln -fs $VI_LOCATION $HOME_EDITOR
     elif [ "$NANO_LOCATION" ]; then
         ln -fs $NANO_LOCATION $HOME_EDITOR
+    elif [ "$PICO_LOCATION" ]; then
+        ln -fs $PICO_LOCATION $HOME_EDITOR
     else:
-        echo 'ERROR: No editors found. Good luck!' >/dev/stderr
+        echo 'ERROR: No editors found. Good luck with sed, cat, and heredoc!' >2
     fi
-
 fi
 
-[[ -f "$HOME_EDITOR" ]] && export EDITOR=$HOME_EDITOR
+[ -f "$HOME_EDITOR" ] && export EDITOR="$(os_readlink_safe $HOME_EDITOR)"
