15d15814431510da00442b7ba029a743394c91f8
Brian Cline <brian.cline@gmail.com>
2014-04-12 13:24:50 -0500

    Add os_readlink_safe, to quiet stderr output

--- common.sh
+++ common.sh
@@ -1,3 +1,16 @@
+os_readlink_safe () {
+    local real_path="$(os_readlink "$1" 2>/dev/null)"
+
+    ## the real location
+    [ -n "$real_path" ] && echo $real_path && return
+
+    ## the input, if it's an existing file or dir
+    ( [ -f "$1" ] || [ -d "$1" ] ) && echo $1 && return
+
+    ## must not exist (or is a type we don't care about)
+    return 1
+}
+
 _detect_platform () {
     if [[ -f /etc/lsb-release ]]; then
         PLATFORM=$(source /etc/lsb-release; echo ${DISTRIB_ID} | awk '{print tolower($0)}')
