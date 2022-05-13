7da177be54cc6b654d6495ac28211f9e9d8fe2a9
Brian Cline <brian.cline@gmail.com>
2022-05-13 18:42:00 -0500

    Ummmm. No comment

--- zsh/wsl.sh
+++ zsh/wsl.sh
@@ -52,8 +52,3 @@ if [[ "$(cat /proc/$PPID/comm)" =~ 'terminator' ]]; then
     pushd -q $HOME
     dirs -c
 fi
-
-
-echo "********************************************************************************"
-echo "****** REMINDER: Change WSL2 local uid to 10000 so you can write via NFS! ******"
-echo "********************************************************************************"
